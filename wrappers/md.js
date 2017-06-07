f80a1ae803e6aef989fb7fb43f224718ac51929a
zhangboxuan <zhangbx@ivymobi.com>
2017-06-08 02:21:54 +0800

    最后一个组件

--- wrappers/md.js
+++ wrappers/md.js
@@ -2,10 +2,14 @@ import React from 'react'
 import moment from 'moment'
 import $ from 'jquery';
 import { Link } from 'react-router';
+import { Anchor } from 'antd';
+import cloneDeep from 'lodash/cloneDeep'
 import Helmet from "react-helmet"
 import ReadNext from '../components/ReadNext'
 import { config } from 'config'
 
+
+import 'antd/lib/anchor/style';
 import '../scss/zenburn.scss'
 import '../scss/container.scss'
 
@@ -19,14 +23,14 @@ class MarkdownWrapper extends React.Component {
     //这里暂时只支持一级标题
     //有一个标题列表，按层级1~6来嵌套
 
-    var _headingObj = {
-      H1: "",
-      H2: "",
-      H3: "",
-      H4: "",
-      H5: "",
-      H6: ""
-    }
+    // var _headingObj = {
+    //   H1: "",
+    //   H2: "",
+    //   H3: "",
+    //   H4: "",
+    //   H5: "",
+    //   H6: ""
+    // }
     var _tree = {};
     var _num = 0;
     var _isFirst = true;
@@ -41,44 +45,169 @@ class MarkdownWrapper extends React.Component {
       // console.info("_nextHeading[0].nodeName===>", _nextHeading[0].nodeName)
       // console.info("_nextHeading[0].innerText===>", _nextHeading[0].innerText)
       // console.info("--------------------------");
-      _num++;
-      if (_num > 10) {
-        break;
-      }
+      // _num++;
+      // if (_num > 10) {
+      //   break;
+      // }
 
       if (_isFirst) {
-        _headingObj[_nextHeading[0].nodeName] = _nextHeading[0].id;
-        _tree = _nextHeading[0];
+        // _headingObj[_nextHeading[0].nodeName] = _nextHeading[0].id;
+        _tree = $(_nextHeading).clone();
         _isFirst = false;
-        _lastNodeName = _tree.nodeName;
+        _lastNodeName = _tree[0].nodeName;
       } else {
         // console.info(_nodeList[_nodeList.length - 1]);
-        debugger;
-        if (_tree.nodeName >= _nextHeading[0].nodeName) {
+        if (_tree[0].nodeName >= _nextHeading[0].nodeName) {
+
           _treeList.push(_tree);
-          _tree = _nextHeading[0];
-          // _lastNodeName = _tree.nodeName;
+          _tree = _nextHeading;
+          // _lastNodeName = _tree[0].nodeName;
         } else {
-          var _tmpNode = _nextHeading[0];
-          _tree = this.getTree(_tree, _tmpNode);
+
+          // debugger;
+          var _tmpNode = $(_nextHeading).clone();
+          this.isPush = false;
+          if(!this.isPush) {
+            _tree = this.getTree1(_tree, _tmpNode);
+          }
+          if(!this.isPush) {
+            _tree = this.getTree2(_tree, _tmpNode);
+          }
+          if(!this.isPush) {
+            _tree = this.getTree3(_tree, _tmpNode);
+          }
+          if(!this.isPush) {
+            _tree = this.getTree4(_tree, _tmpNode);
+          }
+          if(!this.isPush) {
+            _tree = this.getTree5(_tree, _tmpNode);
+          }
+
         }
       }
     }
     console.info("_treeList==>", _treeList);
+    _treeList.map((data) => {
+      if (data.treeNodes) {
+        _nodeList.push(this.createTree(data));
+      } else {
+        _nodeList.push(this.createNode(data));
+      }
+    })
+
+    console.info(_nodeList);
+    this.state.navList = _nodeList;
   }
 
-  getTree (parentNode, childNode) {
-    if (!parentNode.treeNodes) {
-      parentNode.treeNodes = [childNode];
-      return parentNode;
-    } else if(parentNode.treeNodes && parentNode.treeNodes.length > 0) {
-      if (parentNode.treeNodes[parentNode.treeNodes.length - 1].nodeName >= childNode.nodeName) {
-        this.getTree(parentNode.treeNodes[parentNode.treeNodes.length - 1], childNode);
-      } else {
-        parentNode.treeNodes.push(childNode);
-        return parentNode;
+  renderNavList() {
+    return (
+      <Anchor className="nav-anchor">
+        {
+          this.state.navList.map((_nav) => {
+            return (
+              _nav
+            )
+          })
+        }
+      </Anchor>
+    )
+  }
+
+  createNode (data) {
+
+    if (data.treeNodes) {
+      return (
+        <Anchor.Link href={`#${data[0].id}`} title={data[0].innerText}>{this.createTree(data)}</Anchor.Link>
+      )
+    } else {
+      return <Anchor.Link href={`#${data[0].id}`} title={data[0].innerText} />
+    }
+  }
+
+  createTree (data) {
+    // debugger;
+    console.info("createTree==>", data);
+    return (
+      data.treeNodes.map((node) => {
+          return this.createNode(node);
+      })
+    )
+  }
+
+  getTree1 (parentNode, childNode) {
+    var l1ParentNode = parentNode;
+    if (!l1ParentNode.treeNodes) {
+      l1ParentNode.treeNodes = [childNode];
+      this.isPush = true;
+    } else if(l1ParentNode.treeNodes && l1ParentNode.treeNodes.length > 0) {
+      if (l1ParentNode.treeNodes[l1ParentNode.treeNodes.length - 1][0].nodeName >= childNode[0].nodeName) {
+        l1ParentNode.treeNodes.push(childNode);
+        this.isPush = true;
       }
     }
+    return parentNode;
+  }
+
+  getTree2 (parentNode, childNode) {
+    var l2ParentNode = parentNode.treeNodes[parentNode.treeNodes.length - 1];
+    if (!l2ParentNode.treeNodes) {
+      l2ParentNode.treeNodes = [childNode];
+      this.isPush = true;
+    } else if(l2ParentNode.treeNodes && l2ParentNode.treeNodes.length > 0) {
+      if (l2ParentNode.treeNodes[l2ParentNode.treeNodes.length - 1][0].nodeName >= childNode[0].nodeName) {
+        l2ParentNode.treeNodes.push(childNode);
+        this.isPush = true;
+      }
+    }
+    return parentNode;
+  }
+
+  getTree3 (parentNode, childNode) {
+    var l3ParentNode = parentNode.treeNodes[parentNode.treeNodes.length - 1];
+    var l2ParentNode = l3ParentNode.treeNodes[l3ParentNode.treeNodes.length - 1];
+    if (!l2ParentNode.treeNodes) {
+      l2ParentNode.treeNodes = [childNode];
+      this.isPush = true;
+    } else if(l2ParentNode.treeNodes && l2ParentNode.treeNodes.length > 0) {
+      if (l2ParentNode.treeNodes[l2ParentNode.treeNodes.length - 1][0].nodeName >= childNode[0].nodeName) {
+        l2ParentNode.treeNodes.push(childNode);
+        this.isPush = true;
+      }
+    }
+    return parentNode;
+  }
+
+  getTree4 (parentNode, childNode) {
+    var l4ParentNode = parentNode.treeNodes[parentNode.treeNodes.length - 1];
+    var l3ParentNode = l4ParentNode.treeNodes[l4ParentNode.treeNodes.length - 1];
+    var l2ParentNode = l3ParentNode.treeNodes[l3ParentNode.treeNodes.length - 1];
+    if (!l2ParentNode.treeNodes) {
+      l2ParentNode.treeNodes = [childNode];
+      this.isPush = true;
+    } else if(l2ParentNode.treeNodes && l2ParentNode.treeNodes.length > 0) {
+      if (l2ParentNode.treeNodes[l2ParentNode.treeNodes.length - 1][0].nodeName >= childNode[0].nodeName) {
+        l2ParentNode.treeNodes.push(childNode);
+        this.isPush = true;
+      }
+    }
+    return parentNode;
+  }
+
+  getTree5 (parentNode, childNode) {
+    var l5ParentNode = parentNode.treeNodes[parentNode.treeNodes.length - 1];
+    var l4ParentNode = l5ParentNode.treeNodes[l5ParentNode.treeNodes.length - 1];
+    var l3ParentNode = l4ParentNode.treeNodes[l4ParentNode.treeNodes.length - 1];
+    var l2ParentNode = l3ParentNode.treeNodes[l3ParentNode.treeNodes.length - 1];
+    if (!l2ParentNode.treeNodes) {
+      l2ParentNode.treeNodes = [childNode];
+      this.isPush = true;
+    } else if(l2ParentNode.treeNodes && l2ParentNode.treeNodes.length > 0) {
+      if (l2ParentNode.treeNodes[l2ParentNode.treeNodes.length - 1][0].nodeName >= childNode[0].nodeName) {
+        l2ParentNode.treeNodes.push(childNode);
+        this.isPush = true;
+      }
+    }
+    return parentNode;
   }
 
   render () {
@@ -90,6 +219,7 @@ class MarkdownWrapper extends React.Component {
         <Helmet
           title={`${post.title} | ${config.blogTitle}`}
         />
+        {this.renderNavList()}
         <h1 style={{marginTop: 0}}>{post.title}</h1>
         <div className="artical-msg">
           <span>发表时间：{moment(post.date).format('MMMM D, YYYY')}</span>|
